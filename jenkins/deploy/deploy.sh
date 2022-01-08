123806a29287b0a5c4864193046dfab25d1136da
root <root@localhost.localdomain>
2022-01-08 09:31:14 -0500

    Jenkinsfile change and deploy

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -3,8 +3,8 @@
 echo maven-demo-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 
-scp -i /root/jenkins-data/pipeline/prod /tmp/.auth prod-user@18.191.154.231:/tmp/.auth
+scp -i /opt/prod /tmp/.auth prod-user@18.191.154.231:/tmp/.auth
 
-scp -i  /root/jenkins-data/pipeline/prod ./jenkins/deploy/publish prod-user@18.191.154.231:/tmp/publish
+scp -i /opt/prod ./jenkins/deploy/publish prod-user@18.191.154.231:/tmp/publish
 
-ssh -i /root/jenkins-data/pipeline/prod prod-user@18.191.154.231 "/tmp/publish"
+ssh -i /opt/prod prod-user@18.191.154.231 "/tmp/publish"
