098f35807ba6c4867e2204a3e08950739fcc932e
root <root@localhost.localdomain>
2022-01-08 07:53:01 -0500

    Fixes workspace path

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,4 +4,6 @@ echo "*******************************"
 echo "****Building Jar Darshan*******"
 echo "*******************************"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.5.4-alpine mvn -B -DskipTests clean package
+WORKSPACE=/root/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.5.4-alpine mvn -B -DskipTests clean package
